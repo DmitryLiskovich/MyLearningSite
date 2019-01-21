aff63fc08329db9b385034ec7d593f0ec09915e8
Dmitry Liskovich <Dima_liskovich@mail.ru>
2019-01-21 20:30:54 +0300

    21012019

--- js/imgslide.js
+++ js/imgslide.js
@@ -1,16 +1,15 @@
-
 var i = 0;
 
-function slide(){
+function slider(){
     let imagesUrl = ["Img/Header-img.jpg",
     "Img/Hi1.jpg", 
     "Img/Hi2.jpg",
     "Img/Hi3.jpg"];
-    let currentImg = document.getElementById("images");
+    let currentImg = document.getElementsByClassName("image-slider");
     i > 3 ? i = 0 : i;
-    currentImg.src = imagesUrl[i];
+    currentImg[0].src = imagesUrl[i];
     i++;
-    setTimeout("slide()", 5000);
+    setTimeout("slider()", 5000);
 }
 
-slide();
+slider();
